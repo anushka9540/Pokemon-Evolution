eb697d348badf436ac5cc5fe646563421a535ba8
Anushka Prajapati <anushkaprajapati827@gmail.com>
2025-03-11 19:03:52 +0530

    fixed pr

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,75 @@
 <template>
   <div class="container">
     <h1>Pokémon Evolution</h1>
-    <div v-if="loading" class="loader">Loading Pokémon...</div>
-    <PokemonCards v-else :pokemons="pokemons" @select="fetchEvolutions" :selectedPokemon="selectedPokemon" />
-
-    <div v-if="evolutionChain.length" class="evolution-container">
-      <h2>Evolution Chain of {{ selectedPokemon?.name }}</h2>
-      <div v-if="evolutionLoading" class="loader">Loading Evolution...</div>
-      <PokemonCards v-else :pokemons="evolutionChain" />
+  
+    <div v-if="loading" class="loader">
+      <img :src="loader" alt="Loading..." />
+    </div>
+  
+    <PokemonCards v-if="!loading" :pokemons="pokemons" @select="fetchEvolutions" :selectedPokemon="selectedPokemon" />
+  
+    <div class="evolution-container">
+      <h2 v-if="selectedPokemon">
+        Evolution Chain of {{ selectedPokemon.name }}
+      </h2>
+  
+      <div v-if="evolutionLoading" class="loader">
+        <img :src="loader" alt="Loading..." />
+      </div>
+  
+      <PokemonCards v-if="!evolutionLoading && evolutionChain.length" :pokemons="evolutionChain" />
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import PokemonCards from './components/PokemonCard.vue';
+import PokemonCards from './components/PokemonCards.vue';
+import loader from './assets/loader.gif';
 
 const pokemons = ref([]);
 const evolutionChain = ref([]);
 const selectedPokemon = ref(null);
-const loading = ref(false);
+const loading = ref(true);
 const evolutionLoading = ref(false);
-const apiCache = new Map();
 const initialPokemonIds = [1, 4, 7];
 
-const fetchCachedData = async (url) => {
-  if (apiCache.has(url)) return apiCache.get(url);
-  const response = await fetch(url);
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+const fetchPokemonData = async (id) => {
+  const response = await fetch(`${BASE_URL}${id}`);
   const data = await response.json();
-  apiCache.set(url, data);
-  return data;
-};
-
-const fetchPokemons = async () => {
-  if (loading.value) return;
-  loading.value = true;
-
-  pokemons.value = await Promise.all(
-    initialPokemonIds.map(async (id) => {
-      const data = await fetchCachedData(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      return {
-        id: data.id,
-        name: data.name,
-        image: data.sprites.other['official-artwork'].front_default,
-        types: data.types.map((t) => t.type.name)
-      };
-    })
-  );
-
-  loading.value = false;
-};
-
-const fetchEvolutions = async (pokemon) => {
-  if (selectedPokemon.value?.id === pokemon.id || evolutionLoading.value) return;
-  selectedPokemon.value = pokemon;
-  evolutionLoading.value = true;
-
-  try {
-    
-    const evolutionIds = [pokemon.id, pokemon.id + 1, pokemon.id + 2];
-
-    evolutionChain.value = await Promise.all(
-      evolutionIds.map(fetchCachedPokemonData)
-    );
-  } catch (error) {
-    console.error('Error fetching evolution data:', error);
-  } finally {
-    evolutionLoading.value = false;
-  }
-};
-
-const fetchCachedPokemonData = async (id) => {
-  const data = await fetchCachedData(`https://pokeapi.co/api/v2/pokemon/${id}`);
   return {
     id: data.id,
     name: data.name,
     image: data.sprites.other['official-artwork'].front_default,
     types: data.types.map((t) => t.type.name)
   };
+};
+
+const fetchPokemons = async () => {
+  loading.value = true;
+  pokemons.value = await Promise.all(initialPokemonIds.map(fetchPokemonData));
+  loading.value = false;
+};
+
+const fetchEvolutions = async (pokemon) => {
+  if (selectedPokemon.value?.id === pokemon.id || evolutionLoading.value)
+    return;
+  selectedPokemon.value = pokemon;
+  evolutionLoading.value = true;
+  evolutionChain.value = [];
+
+  try {
+    const evolutionIds = [pokemon.id + 1, pokemon.id + 2];
+    evolutionChain.value = await Promise.all(
+      evolutionIds.map(fetchPokemonData)
+    );
+  } catch (error) {
+    console.error('Error fetching evolution data:', error);
+  } finally {
+    evolutionLoading.value = false;
+  }
 };
 
 onMounted(fetchPokemons);
@@ -95,9 +87,11 @@ onMounted(fetchPokemons);
 }
 
 .loader {
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #f8d030;
   margin: 20px 0;
+}
+
+.loader img {
+  width: 50px;
+  height: 50px;
 }
 </style>
