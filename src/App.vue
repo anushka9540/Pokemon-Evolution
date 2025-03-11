21ed77b692765ea072ca18d738660e9333995afe
Anushka Prajapati <anushkaprajapati827@gmail.com>
2025-03-11 11:09:20 +0530

    removed routing through home component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1>Pokémon Evolution</h1>
     <PokemonCards :pokemons="pokemons" @select="fetchEvolutions" :selectedPokemon="selectedPokemon" />
-
+  
     <div v-if="evolutionChain.length" class="evolution-container">
       <h2>Evolution Chain of {{ selectedPokemon?.name }}</h2>
       <PokemonCards :pokemons="evolutionChain" />
@@ -11,8 +11,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import PokemonCards from "./components/PokemonCard.vue";
+import { ref, onMounted } from 'vue';
+import PokemonCards from './components/PokemonCard.vue';
 
 const pokemons = ref([]);
 const evolutionChain = ref([]);
@@ -20,52 +20,61 @@ const selectedPokemon = ref(null);
 
 const fetchPokemons = async () => {
   const urls = [
-    "https://pokeapi.co/api/v2/pokemon/1",
-    "https://pokeapi.co/api/v2/pokemon/4",
-    "https://pokeapi.co/api/v2/pokemon/7",
+    'https://pokeapi.co/api/v2/pokemon/1',
+    'https://pokeapi.co/api/v2/pokemon/4',
+    'https://pokeapi.co/api/v2/pokemon/7'
   ];
 
-  const responses = await Promise.all(urls.map(url => fetch(url).then(res => res.json())));
-  pokemons.value = responses.map(res => ({
+  const responses = await Promise.all(
+    urls.map((url) => fetch(url).then((res) => res.json()))
+  );
+  pokemons.value = responses.map((res) => ({
     id: res.id,
     name: res.name,
-    image: res.sprites.other["official-artwork"].front_default,
-    types: res.types.map(t => t.type.name),
-    speciesUrl: res.species.url,
+    image: res.sprites.other['official-artwork'].front_default,
+    types: res.types.map((t) => t.type.name),
+    speciesId: res.species.url.split('/').slice(-2, -1)[0]
   }));
 };
 
 const fetchEvolutions = async (pokemon) => {
   evolutionChain.value = [];
-  selectedPokemon.value = pokemon; 
+  selectedPokemon.value = pokemon;
 
-  const speciesRes = await fetch(pokemon.speciesUrl);
-  const speciesData = await speciesRes.json();
-  const evolutionRes = await fetch(speciesData.evolution_chain.url);
-  const evolutionData = await evolutionRes.json();
+  try {
+    const speciesRes = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`
+    );
+    const speciesData = await speciesRes.json();
 
-  let chain = evolutionData.chain;
-  let evoList = [];
+    const evolutionRes = await fetch(speciesData.evolution_chain.url);
+    const evolutionData = await evolutionRes.json();
 
-  while (chain) {
-    evoList.push(chain.species.name);
-    chain = chain.evolves_to.length ? chain.evolves_to[0] : null;
+    let chain = evolutionData.chain;
+    let evoList = [];
+
+    while (chain) {
+      evoList.push(chain.species.url.split('/').slice(-2, -1)[0]);
+      chain = chain.evolves_to.length ? chain.evolves_to[0] : null;
+    }
+
+    const evoData = await Promise.all(
+      evoList.map(async (id) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await res.json();
+        return {
+          id: data.id,
+          name: data.name,
+          image: data.sprites.other['official-artwork'].front_default,
+          types: data.types.map((t) => t.type.name)
+        };
+      })
+    );
+
+    evolutionChain.value = evoData;
+  } catch (error) {
+    console.error('Error fetching evolution data:', error);
   }
-
-  const evoData = await Promise.all(
-    evoList.map(async (name) => {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      const data = await res.json();
-      return {
-        id: data.id,
-        name: data.name,
-        image: data.sprites.other["official-artwork"].front_default,
-        types: data.types.map(t => t.type.name),
-      };
-    })
-  );
-
-  evolutionChain.value = evoData;
 };
 
 onMounted(fetchPokemons);
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import App from "../App.vue";
 
 const routes = [
-  { path: "/", component: Home }
+  { path: "/", component: App }
 ];
 
 const router = createRouter({
